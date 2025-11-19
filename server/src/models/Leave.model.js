77c9a3db23800eb57b62fc0de4848d86528a238c
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-11-20 00:24:59 +0530

    leave model

---
+++ server/src/models/Leave.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const leaveSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    leaveType: {
+      type: String,
+      enum: ["short", "full_day"],
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      default: Date.now,
+    },
+    endDate: {
+      type: Date,
+    },
+    startTime: {
+      type: String,
+    },
+    endTime: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+leaveSchema.index({ user: 1 });
+
+const Leave = mongoose.model("Leave", leaveSchema);
+
+export default Leave;
