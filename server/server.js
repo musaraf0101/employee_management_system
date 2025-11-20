ac7e7e0e81b7c4f9b5c036c9bb1fbf1d5d1a6c69
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-11-20 10:41:01 +0530

    cors allowed

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRouter from "./src/routes/auth.route.js";
 import { DBConnection } from "./src/config/db.js";
 
@@ -9,6 +10,12 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api", authRouter);
 
