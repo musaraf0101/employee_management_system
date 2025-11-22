a71118feca0f715652a976c08c28d6e00f116131
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-11-22 13:43:32 +0530

    cors allowed

--- server/server.js
+++ server/server.js
@@ -11,14 +11,18 @@ import reportRouter from "./src/routes/report.route.js";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
+
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", authRouter);
 app.use("/api", userRouter);
