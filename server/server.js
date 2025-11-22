0aa413c84b294fb09f4e246f90312c4493d1e41c
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-11-22 16:47:20 +0530

    production

--- server/server.js
+++ server/server.js
@@ -7,19 +7,23 @@ import { DBConnection } from "./src/config/db.js";
 import userRouter from "./src/routes/user.route.js";
 import leaveRouter from "./src/routes/leave.route.js";
 import reportRouter from "./src/routes/report.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
+}
 
 app.use(express.json());
 app.use(cookieParser());
@@ -28,6 +32,14 @@ app.use("/api", authRouter);
 app.use("/api", userRouter);
 app.use("/api", leaveRouter);
 app.use("/api", reportRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+  
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
 
 DBConnection();
 app.listen(3000, () => {
