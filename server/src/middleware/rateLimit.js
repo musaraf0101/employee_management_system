d72c1d98cc9dd5a58e4798aac762ed987933d2cc
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-11-24 11:02:39 +0530

    rate limit changes

--- server/src/middleware/rateLimit.js
+++ server/src/middleware/rateLimit.js
@@ -1,12 +1,12 @@
 import rateLimit from "express-rate-limit";
 
 export const loginLimit = rateLimit({
-  windowMs: 60 * 1000,
-  max: 10,
+  windowMs: 60 * 60 * 1000,
+  max: 50,
   handler: (req, res) => {
     res.status(429).json({
       success: false,
-      message: "Too many request. Try again after 1 minutes",
+      message: "Too many request. Try again after 1 hours",
     });
   },
 });
